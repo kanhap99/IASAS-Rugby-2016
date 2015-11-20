3f28a0e7738453c28162a0c28afd0afd3b37e16e
PRASAD Kanha <41562@2017-41562.local>
2015-11-20 22:37:55 +0700

    Fonts + img

--- routes.py
+++ routes.py
@@ -4,8 +4,10 @@ from flask import render_template
 app = Flask(__name__);
 
 @app.route('/')
-def hello():
+def home():
     return render_template('template.html')
-
+@app.route('/teams/<school>')
+def showTeam(school):
+    return render_template(school+'.html')
 if __name__ == '__main__':
     app.run()
