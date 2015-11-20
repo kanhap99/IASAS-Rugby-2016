e6346419e9244d09dccda7ca4f35490074aeb25e
PRASAD Kanha <41562@2017-41562.local>
2015-11-20 22:56:12 +0700

    removed folders

--- routes.py
+++ routes.py
@@ -5,9 +5,12 @@ app = Flask(__name__);
 
 @app.route('/')
 def home():
-    return render_template('template.html')
+    return render_template('home.html')
+
 @app.route('/teams/<school>')
 def showTeam(school):
-    return render_template(school+'.html')
+    return 'Welcome to JIS '+ str(school)
+
+
 if __name__ == '__main__':
     app.run()
