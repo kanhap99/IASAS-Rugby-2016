3b1195f14dea1609f813fe5122e290a2f7e9b308
PRASAD Kanha <41562@2017-41562.local>
2015-11-20 23:17:19 +0700

    added templates

--- routes.py
+++ routes.py
@@ -6,11 +6,14 @@ app = Flask(__name__);
 @app.route('/')
 def home():
     return render_template('home.html')
-
 @app.route('/teams/<school>')
-def showTeam(school):
-    return 'Welcome to JIS '+ str(school)
-
-
+def dispTeam(school):
+    return render_template(school+'.html')
+@app.route('/schedule')
+def dispSchedule():
+    return render_template('schedule.html')
+@app.route('/results/<resultGender>')
+def dispResults(resultGender):
+    return render_template(resultGender+'Results.html')
 if __name__ == '__main__':
     app.run()
